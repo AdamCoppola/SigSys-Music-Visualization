2a7bc63ecb561911065a21c10b97cf177829fc4a
EnigMoiD <evan.dorsky@icloud.com>
2014-04-24 15:03:50 -0400

    Data processing working, plotting broken

--- python/signal.py
+++ python/signal.py
@@ -1,16 +1,36 @@
-import numpy as np
-
+from numpy import *
 import scipy.io.wavfile as wio
-
 from scipy import signal, misc
-
 import matplotlib.pyplot as plt
 
-t = np.linspace(0, 5, 1000)
-sigchirp = signal.chirp(t, 10, 3, 15, method='logarithmic')
+import time
 
-plt.plot(t, sigchirp)
+plt.ion()
 
-wio.write('chirp.wav', 40000, sigchirp)
+def process (data, window):
+	windows = len(data)/window
+	for i in xrange(0, windows-2):
+		transformed = transform(data[i*window:(i+1)*window])
+		plotFFT(transformed)
 
-plt.show()
+def makeEven (num):
+	if num % 2:
+		return num-1
+	else:
+		return num
+
+def transform (data):
+	left,right = split(abs(fft.fft(data)),2)
+	ys = add(left,right[::-1])
+	return ys
+
+def plotFFT (data):
+	plt.plot(data, hold="on")
+	plt.show()
+	time.sleep(.1)
+	plt.clf()
+
+rate, audio = wio.read('../wav/pompeii.wav')
+laudio, raudio = zip(*audio)
+
+process(laudio, makeEven(rate))
