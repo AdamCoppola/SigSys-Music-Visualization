22e49c7fafe441f088af44bd417237875f3090e1
EnigMoiD <evan.dorsky@icloud.com>
2014-04-26 13:30:37 -0400

    Displays rudimentary spectrogram

--- python/signal.py
+++ python/signal.py
@@ -6,34 +6,34 @@ import matplotlib.animation as anim
 
 import time
 
-def updatePlot (handle, data):
-	handle.set_ydata(data)
-	return handle
-
-def process (data, window):
-	windows = len(data)/window
-	for i in xrange(0, windows-2):
-		transformed = transform(data[i*window:(i+1)*window])
-		plotFFT(transformed)
-
 def makeEven (num):
 	if num % 2:
 		return num-1
 	else:
 		return num
 
+def process (data, window):
+	windows = len(data)/window
+	print 'WINDOWS'
+	print windows
+	spectrogram = empty(shape=(windows, window/2))
+	for i in xrange(0, windows-2):
+		spectrogram[i]=transform(data[i*window:(i+1)*window])
+	return spectrogram
+
 def transform (data):
 	left,right = split(abs(fft.fft(data)),2)
 	ys = add(left,right[::-1])
 	return ys
 
-def plotFFT (data):
-	plt.plot(data, hold="on")
-	plt.show()
-	time.sleep(.1)
-	plt.clf()
-
 rate, audio = wio.read('../wav/pompeii.wav')
+print "Done reading"
 laudio, raudio = zip(*audio)
+print "Done zipping"
 
-process(laudio, makeEven(rate))
+spec = process(laudio, makeEven(rate))
+
+plt.figure(1)
+plt.matshow(spec, 1)
+
+plt.show()
